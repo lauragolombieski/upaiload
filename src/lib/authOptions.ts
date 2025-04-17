052ef11c6003bcb2d67c9eb254a0cdde11ccd208
lauragolombieski <lauragolombieskib@gmail.com>
2025-04-17 04:07:52 -0300

    Dev - fix: Ajustes

---
+++ src/lib/authOptions.ts
@@ -0,0 +1,44 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Senha", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email || "" },
+        });
+
+        if (user && user.password && await bcrypt.compare(credentials?.password || '', user.password)) {
+          return { id: user.id, email: user.email };
+        }
+
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
+};
